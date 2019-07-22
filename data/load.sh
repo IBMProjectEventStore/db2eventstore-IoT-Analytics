eb7e8e6ec6b2f34eb6542445597c93abf38bc85b
gigglegrig <xi.sun@ibm.com>
2019-07-22 12:31:28 -0400

    Frank/migration to2.0 (#3)
    * init drop
    
    * .gitignore
    
    * NFS server set-up and loading script.
    
    * typo
    
    * chmod nfs_setup.sh
    
    * change testdb to eventdb
    
    * rest finished

--- data/load.sh
+++ data/load.sh
@@ -1,14 +1,63 @@
-#!/bin/bash
+#!/bin/bash -x
 
-if [[ $(java -version 2>&1) != *'version'* ]]; then
-    echo "Java not found. Please install Java before continuing."
-	exit 1
+NAMESPACE="dsx"
+RELEASE_NAME="eventstore"
+DATA_PATH="/ibm"
+
+function usage()
+{
+cat <<-USAGE #| fmt
+Description:
+This script should be run after NFS_setup.sh script. It will ensure that the
+target external_db mount point is properly mounted with NFS partition. Then it 
+calls the ingest.clp created by NFS_setup.sh script to load the data.
+
+-----------
+Usage: $0 [OPTIONS] [arg]
+OPTIONS:
+========
+-n|--namespace    [Default: dsx] Kubernetes namespace that Event Store is deployed under.
+USAGE
+}
+
+while [ -n "$1" ]; do
+    case "$1" in
+    -h|--help)
+        usage >&2
+        exit 0
+        ;;
+    -n|--namespace)
+        NAMESPACE="$1"
+        ;;
+    *)
+        echo "Unknown option:$1"
+        usage >&2
+        exit 1
+    esac
+done
+
+if [ ${NAMESPACE} != "dsx" ]; then
+    RELEASE_NAME=`helm ls --tls | grep db2eventstore | grep -v catalog | awk {'print $1'} | uniq`
+    DATA_PATH="/data"
 fi
 
-if [ ! -f ./es-csvloader.jar ]; then
-	echo "\"es-csvloader.jar\" missing. Downloading \"es-csvloader.jar\""
-	curl -o ./es-csvloader.jar 'https://oss.sonatype.org/content/groups/public/com/ibm/event/es-csvloader/1.0.0/es-csvloader-1.0.0.jar'
-fi
+# check nfs mount on each node for each instance
+for node in ${nodes}; do
+    for release in ${RELEASE_NAME}; do
+        if [ $(findmnt -T ${DATA_PATH}/${release}/engine/utils/external_db | grep nfs) ]; then
+            echo "Error: NFS server not mounted on ${DATA_PATH}/${release}/engine/utils/external_db/"
+            echo "Please ensure Event Store is properly installed and"
+            echo "NFS partition is mount on the above path on node $node"
+            exit 1
+        fi
+    done
+done
 
-java -jar es-csvloader.jar "$@"
+# define eventstoreUtils macro
+TOOLS_CONTAINER=$(kubectl get pods -n "$NAMESPACE" | grep eventstore-tenant-tools | \
+    awk {'print $1'}) && ORIGIN_IP=`hostname -i | awk '{print $1}'`
+eventstoreUtils() { kubectl -n "$NAMESPACE" exec "${TOOLS_CONTAINER}" \
+    -- bash -c 'export ORIGIN_IP="${1}" && eventstore-utils "${@:2}"' bash "${ORIGIN_IP}" "${@}"; }
 
+# execute external table loading
+eventstoreUtils --tool db2_engine --command "db2 -f /eventstore/db/external_db/ingest.clp" | tee
--- rest/test.js
+++ rest/test.js
@@ -113,6 +113,8 @@ function callESAPIWithBody (eventStoreURL, method, token, body, flow) {
         process.exit()
       }
       displayOutput (server + eventStoreNamespace + eventStoreURL, error, response, body)
+      console.log('')
+      console.log('**Query Result:**')
       console.log(JSON.parse(body).data);
       var callback = flow.shift()
       if (callback != undefined) {
@@ -161,7 +163,7 @@ function callAPI (label, url, method, token, flow) {
 // === End Generic API to call the event store ===
 
 // === SPARK SQL Support ===
-function submitSparkQuery2 (token, flow) {
+function submitSparkQuery1 (token, flow) {
   console.log ('')
   console.log ('Submitting a SparkSql query')
   var query = 'select count(*) as count from IOT_TEMP where deviceID=1 and sensorID=31'
@@ -169,17 +171,17 @@ function submitSparkQuery2 (token, flow) {
   console.log ('Submitting -> \"' + query + '\"')
   console.log ('Result of SparkSql query:')
   var body = "{\"sql\":\"" + query + "\"}"
-  callESAPIWithBody('/spark/sql?tableName=IOT_TEMP&databaseName=TESTDB', 'POST', token, body, flow)
+  callESAPIWithBody('/spark/sql?tableName=IOT_TEMP&databaseName=EVENTDB', 'POST', token, body, flow)
 }
-function submitSparkQuery1 (token, flow) {
+function submitSparkQuery2 (token, flow) {
   console.log ('')
   console.log ('Submitting a SparkSql query')
-  var query = 'select * from IOT_TEMP where deviceID=1 and sensorID=31'
+  var query = 'select * from IOT_TEMP where deviceID=1 and sensorID=31 limit 5'
   console.log ('==========================')
   console.log ('Submitting -> \"' + query + '\"')
   console.log ('Result of SparkSql query:')
   var body = "{\"sql\":\"" + query + "\"}"
-  callESAPIWithBody('/spark/sql?tableName=IOT_TEMP&databaseName=TESTDB', 'POST', token, body, flow)
+  callESAPIWithBody('/spark/sql?tableName=IOT_TEMP&databaseName=EVENTDB', 'POST', token, body, flow)
 }
 // === End SPARK SQL Support ===
 
@@ -188,11 +190,11 @@ function getDatabases (token, flow) {
   callAPI ('Get Database', '/oltp/databases', 'GET', token, flow)
 }
 function getTables (token, flow) {
-  callAPI ('Get the list of Tables', '/oltp/tables?databaseName=TESTDB', 'GET', token, flow)
+  callAPI ('Get the list of Tables', '/oltp/tables?databaseName=EVENTDB', 'GET', token, flow)
 }
 
 function getTableInfo (token, flow) {
-  callAPI ('Get the table Info', '/oltp/table?tableName=IOT_TEMP&databaseName=TESTDB', 'GET', token, flow)
+  callAPI ('Get the table Info', '/oltp/table?tableName=IOT_TEMP&databaseName=EVENTDB', 'GET', token, flow)
 }
 
 // === Connect Support ===
@@ -233,10 +235,10 @@ console.log ('')
 var flow = []
 flow.push(connectEngine)
 flow.push(getDatabases)
-//flow.push(getTableInfo)
+flow.push(getTableInfo)
 //flow.push(getTables)
-flow.push(submitSparkQuery2)
 flow.push(submitSparkQuery1)
+flow.push(submitSparkQuery2)
 
 if (noAuth === undefined) {
   getBearerToken(flow);
