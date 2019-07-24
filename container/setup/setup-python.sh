91b9ebc62fdcad56762e0d6c112ac890895816af
XiSun <xi.sun@ibm.com>
2019-07-24 14:56:43 -0400

    setup python 3.5

--- container/setup/setup-python.sh
+++ container/setup/setup-python.sh
@@ -3,6 +3,18 @@
 ## python
 ##
 
+## Set up python 3.5
+
+cd /usr/src
+wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz
+tar xzf Python-3.5.6.tgz
+cd Python-3.5.6
+./configure --enable-optimizations
+make altinstall
+rm /usr/src Python-3.5.6.tgz
+ln -s /usr/local/bin/python3.5 /usr/local/bin/python
+
+## set up EventStore python library
 if [ -z "${SPARK_HOME}" ]
 then
    SPARK_HOME=/spark_home
