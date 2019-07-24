286817ea433db162a0336c12afa1bda9f13eed58
root <root@forgave1.fyre.ibm.com>
2019-07-24 11:23:22 -0700

    remote retrieve python package

--- container/setup/setup-python.sh
+++ container/setup/setup-python.sh
@@ -9,9 +9,9 @@ then
    echo "SPARK_HOME not defined. Using SPARK_HOME=${SPARK_HOME}"
 fi
 
-unzip /root/python.zip -d /var/lib
-rm -f /root/python.zip
+git clone https://github.com/IBMProjectEventStore/db2eventstore-pythonpackages.git /
+unzip /db2eventstore-pythonpackages/python.zip -d /var/lib
+rm -rf /db2eventstore-pythonpackages
 
 PY4J_VERSION=$(ls ${SPARK_HOME}/python/lib | grep py4j)
-
 echo "export PYTHONPATH=$PYTHONPATH:/var/lib:${SPARK_HOME}/python:${SPARK_HOME}/python/lib/${PY4J_VERSION}" >> /etc/profile.d/local_python.sh
