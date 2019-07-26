733dbdc79e58f40827a1a941c5b9542f3bf0e394
XiSun <xi.sun@ibm.com>
2019-07-26 10:30:17 -0400

    CURL + Time series

--- curl/auth.sh
+++ curl/auth.sh
@@ -1,6 +1,22 @@
+#!/bin/bash
+
 # This is an example of obtaining a authentication token from the cluster. 
 # a json file will be returned with an "accessToken" that can be used
 #example command is 
 #curl -k -X GET "https://9.30.111.222/v1/preauth/validateAuth" -u admin:password
 
-curl -k -X GET "https://$IP/v1/preauth/validateAuth" -u $USER:$PASSWOD | jq
+AUTH_RES=$(curl -k -X GET "https://$IP/v1/preauth/validateAuth" -u $USER:$PASSWORD | python -m json.tool)
+TOKEN=$(curl -k -X GET "https://$IP/v1/preauth/validateAuth" -u $USER:$PASSWORD | python -c "import sys, json; print json.load(sys.stdin)['accessToken']")
+
+cat <<-EOL
+Authentication result is:
+
+${AUTH_RES}
+
+Authentication token is:
+
+${TOKEN}
+
+Please export the token as the 'accessToken' environment variable, then run the './score.sh' script to get prediction
+value using the deployed machine learning model.
+EOL
