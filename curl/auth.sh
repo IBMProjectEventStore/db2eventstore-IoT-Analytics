1f4c4ee2eece5939b8261fb89caa2a3b1b353de8
gigglegrig <xi.sun@ibm.com>
2019-07-26 21:14:45 -0400

    dockeshell (#4)
    * dockershell
    
    * rest/curl
    
    * CURL + Time series
    
    * export keydb info at setup-ssl.sh
    
    * Update setup-python.sh
    
    * dockershell
    
    * dockershell
    
    * clean build
    
    * image build log
    
    * add branch option
    
    * fix log path
    
    * build msg
    
    * typo
    
    * cleanup

--- curl/auth.sh
+++ curl/auth.sh
@@ -1,6 +1,22 @@
+#!/bin/bash
+
 # This is an example of obtaining a authentication token from the cluster. 
 # a json file will be returned with an "accessToken" that can be used
 #example command is 
-#curl -k -X GET "https://192.168.0.1/v1/preauth/validateAuth" -u admin:password
+#curl -k -X GET "https://9.30.111.222/v1/preauth/validateAuth" -u admin:password
 
-curl -k -X GET "https://$CLUSTER_IP/v1/preauth/validateAuth" -u $userid:$password
+AUTH_RES=$(curl -k -X GET "https://$IP/v1/preauth/validateAuth" -u $EVENT_USER:$EVENT_PASSWORD | python -m json.tool)
+TOKEN=$(curl -k -X GET "https://$IP/v1/preauth/validateAuth" -u $EVENT_USER:$EVENT_PASSWORD | python -c "import sys, json; print json.load(sys.stdin)['accessToken']")
+
+cat <<-EOL
+Authentication result is:
+
+${AUTH_RES}
+
+Authentication token is:
+
+${TOKEN}
+
+Please export the token as the 'accessToken' environment variable, then run the './score.sh' script to get prediction
+value using the deployed machine learning model.
+EOL
--- rest/run.sh
+++ rest/run.sh
@@ -1,15 +1,11 @@
 #!/bin/bash -x
 
-CLUSTER_IP=$(ifconfig | sed -n -e 's/ *inet \(9.30[0-9.]\+\) \+netmask.*/\1/p')
-EVENTSTORE_USERID="admin"
-EVENTSTORE_PASSWORD="password"
-
 function usage()
 {
 cat <<-USAGE #| fmt
 Pre-requisite:
 - You have created the table IOT_TEMP by runnign the Notebook examples
-- You have ingested data into the IOT_TEMP table by running /data/load.sh
+- You have ingested data into the IOT_TEMP table 
 - You have run install.sh in this directory to install required packages
 ========
 Description:
@@ -23,9 +19,12 @@ This script will execute multiple REST APIs to:
 Usage: $0 [OPTIONS] [arg]
 OPTIONS:
 ========
---publicIP    [Default: Public IP] Public IP of any cluster node.
---user [Default: admin] User name of Watson Studio Local user who created the IOT_TEMP table
---password [Default: password] Password of Watson Studio Local user who created the IOT_TEMP table
+--IP  Public IP of any cluster node.
+--user User name of Watson Studio Local user who created the IOT_TEMP table
+       [Default: ${EVENT_USER} shell environment variable]
+--password Password of Watson Studio Local user who created the IOT_TEMP table
+       [Default: ${EVENT_PASSWORD} shell environment variable]
+
 USAGE
 }
 
@@ -35,20 +34,41 @@ while [ -n "$1" ]; do
         usage >&2
         exit 0
         ;;
-    --publicIP)
-        CLUSTER_IP="$1"
+    --IP)
+        IP="$2"
+        shift 2
         ;;
     --user)
-        EVENTSTORE_USERID="$1"
+        EVENT_USER="$2"
+        shift 2
         ;;
     --password)
-        EVENTSTORE_PASSWORD="$1"
+        EVENT_PASSWORD="$2"
+        shift 2
         ;;
     *)
-        echo "Unknown option:$1"
+        echo "Unknown option:$2"
         usage >&2
         exit 1
     esac
 done
 
-node test.js --engine=$CLUSTER_IP:1101 --server=https://$CLUSTER_IP:443 --user=$EVENTSTORE_USERID --password=$EVENTSTORE_PASSWORD
+if [ -z ${EVENT_USER} ]; then
+    echo "Error: Please provide the Watson Studio Local user name with --user flag"
+    usage >&2
+    exit 1
+fi
+
+if [ -z ${EVENT_PASSWORD} ]; then
+    echo "Error: Please provide the Watson Studio Local password with --password flag"
+    usage >&2
+    exit 1
+fi
+
+if [ -z ${IP} ]; then
+    echo "Error: Please provide the Event Store server's public IP with --IP flag"
+    usage >&2
+    exit 1
+fi
+
+node test.js --engine=$IP:1101 --server=https://$IP:443 --user=$EVENT_USER --password=$EVENT_PASSWORD
