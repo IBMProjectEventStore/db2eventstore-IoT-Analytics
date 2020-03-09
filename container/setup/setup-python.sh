3e32b2e586baa3951e285036ebe2b55ee8658759
Chen.Gao <Chen.Gao@ibm.com>
2020-03-09 13:09:28 -0700

    fix python package decompress

--- container/setup/setup-python.sh
+++ container/setup/setup-python.sh
@@ -22,7 +22,7 @@ then
 fi
 
 git clone https://github.com/IBMProjectEventStore/db2eventstore-pythonpackages.git /db2eventstore-pythonpackages
-unzip /db2eventstore-pythonpackages/python.zip -d /var/lib
+tar -C /var/lib -xvf /db2eventstore-pythonpackages/python.zip
 rm -rf /db2eventstore-pythonpackages
 
 PY4J_VERSION=$(ls ${SPARK_HOME}/python/lib | grep py4j)
