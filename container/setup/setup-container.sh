8e0200e7c398c452c19aaa072e62e8c5477da79c
robertpangborn <robertpangborn@gmail.com>
2021-01-19 11:51:06 -0800

    added proxy statements to setup container to resolve wget issue I was having but not likely a long term fix

--- container/setup/setup-container.sh
+++ container/setup/setup-container.sh
@@ -7,7 +7,14 @@ else
    ES_VERSION=\"${1}\"
 fi
 
+# set user volume and proxy values
 USER_VOLUME=/root/user_volume
+export http_proxy=http://proxy1.fyre.ibm.com:3128
+export ftp_proxy=http://proxy1.fyre.ibm.com:3128
+export HTTP_PROXY=http://proxy1.fyre.ibm.com:3128
+export FTP_PROXY=http://proxy1.fyre.ibm.com:3128
+export no_proxy=localhost,127.0.0.1,.fyre.ibm.com
+export NO_PROXY=localhost,127.0.0.1,.fyre.ibm.com
 
 # set the version variables
 SPARK_VERSION=$(jq -r .release.${ES_VERSION}.sparkversion $USER_VOLUME/es-releases.json)
