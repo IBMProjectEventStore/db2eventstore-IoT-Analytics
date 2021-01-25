d371d0cd55b1840b30e9f92924f441c68138be9c
robertpangborn <robertpangborn@gmail.com>
2021-01-25 09:00:46 -0800

    moved setup scripts to dockerfile

--- container/setup/setup-container.sh
+++ container/setup/setup-container.sh
@@ -18,14 +18,5 @@ SPARK_CLIENT=$(jq -r .release.${ES_VERSION}.sparkclient $USER_VOLUME/es-releases
 IOT_REPO_PATH="/root/db2eventstore-IoT-Analytics"
 SETUP_AREA=${IOT_REPO_PATH}/container/setup
 
-# set up environments
-
-${SETUP_AREA}/setup-java.sh
-${SETUP_AREA}/setup-spark.sh
-${SETUP_AREA}/setup-scala.sh
-${SETUP_AREA}/setup-python.sh
-${SETUP_AREA}/setup-kafka.sh
-${IOT_REPO_PATH}/rest/install.sh
-
 echo "Downloading Event Store Spark client for selected version, this may take some time"
 wget -P ${SPARK_HOME}/jars https://${SPARK_CLIENT}
