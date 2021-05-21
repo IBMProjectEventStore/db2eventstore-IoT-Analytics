ea14f2b6414066de8961d3cf667a74875433980f
James Stroud <james.jstroud@gmail.com>
2021-05-21 17:27:41 -0400

    fixed .zip is really a .tar file
    I updated https://github.com/IBMProjectEventStore/db2eventstore-pythonpackages and renamed all 3 python.zip files to python.tar files for the 2.0.0.3, 2.0.0.5, and 2.0.1.0 branches.  The python.tar file for 2.0.0.5 and 2.0.1.0 are identical

--- container/setup/setup-python.sh
+++ container/setup/setup-python.sh
@@ -22,7 +22,7 @@ then
 fi
 
 git clone https://github.com/IBMProjectEventStore/db2eventstore-pythonpackages.git /db2eventstore-pythonpackages
-tar -C /var/lib -xvf /db2eventstore-pythonpackages/python.zip
+tar -C /var/lib -xvf /db2eventstore-pythonpackages/python.tar
 rm -rf /db2eventstore-pythonpackages
 
 PY4J_VERSION=$(ls ${SPARK_HOME}/python/lib | grep py4j)
