362d09350a9305c5caa55c341fb29aa76dd4122a
James Stroud <james.jstroud@gmail.com>
2021-06-14 02:00:09 -0400

    update node.js to 14 from 10

--- rest/install.sh
+++ rest/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Installing packages for REST"
-curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install -y nodejs
 npm install request
