106236639b5883a97ab69e88e1cc234d4a8ecb03
James Stroud <james.jstroud@gmail.com>
2021-09-08 10:08:42 -0400

    upgrades npm to latest version after node.js
    upgrades npm to latest version after node.js  install, you may get a warning  like
    ```
    npm notice
    npm notice New minor version of npm available! 7.21.1 -> 7.22.0
    npm notice Changelog: https://github.com/npm/cli/releases/tag/v7.22.0
    npm notice Run npm install -g npm@7.22.0 to update!
    ```
    you will still get this warning but npm will be updated

--- rest/install.sh
+++ rest/install.sh
@@ -3,4 +3,5 @@
 echo "Installing packages for REST"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
 sudo yum install -y nodejs
+sudo npm update -g npm
 npm install request
