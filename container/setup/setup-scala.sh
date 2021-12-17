2d2be529373e936b254261b2f4d1daf7bc62fc11
James Stroud <james.jstroud@gmail.com>
2021-12-16 20:46:44 -0500

    sbt 1.5.7 to 1.5.5 to test 1.5.5
    sbt 1.5.7 & 1.5.6 did not work, will attempt to rebuild again with sbt 1.5.5 to see if the tests work

--- container/setup/setup-scala.sh
+++ container/setup/setup-scala.sh
@@ -21,10 +21,10 @@ rm -f /tmp/scala-${SCALA_VERSION}.${PKGTYPE}
 
 ##
 ## SBT (Simple Build Tool), used with scala
-## dl.bintray.com is going away.  Installs version 1.5.7 of SBT from github.com.
+## dl.bintray.com is going away.  Installs version 1.5.5 of SBT from github.com.
 
-wget -O /tmp/sbt-1.5.7.tgz https://github.com/sbt/sbt/releases/download/v1.5.7/sbt-1.5.7.tgz
-tar xzvf /tmp/sbt-1.5.7.tgz -C /usr/share/
+wget -O /tmp/sbt-1.5.5.tgz https://github.com/sbt/sbt/releases/download/v1.5.5/sbt-1.5.5.tgz
+tar xzvf /tmp/sbt-1.5.5.tgz -C /usr/share/
 ln -s /usr/share/sbt/bin/sbt /usr/bin/sbt
 
 
