f464e283a5bcbe6b7e7b7dafd5b5fd8d3707c38d
James Stroud <james.jstroud@gmail.com>
2021-12-18 00:36:47 -0500

    add echo for debugging & hardcode spark client url

--- container/setup/setup-container.sh
+++ container/setup/setup-container.sh
@@ -26,7 +26,10 @@ ${SETUP_AREA}/setup-spark.sh $SPARK_VERSION
 IOT_REPO_PATH="/root/db2eventstore-IoT-Analytics"
 SETUP_AREA=${IOT_REPO_PATH}/container/setup
 
-echo "Downloading Event Store Spark client for selected version, this may take some time, using hardcoded url"
+echo "Downloading Event Store Spark client for selected version, this may take some time, using hardcoded url and showing variables for Spark home, version & client"
 # command below stopped workeing because https:// was not getting populated
 # wget -P ${SPARK_HOME}/jars https://${SPARK_CLIENT}
+echo "Spark home directory is ${SPARK_HOME}"
+echo "Spark version is ${SPARK_VERSION}"
+echo "Spark client is ${SPARK_CLIENT}"
 wget -P ${SPARK_HOME}/jars  https://repo1.maven.org/maven2/com/ibm/event/ibm-db2-eventstore-client-spark-2.4.6/2.0.1.0/ibm-db2-eventstore-client-spark-2.4.6-2.0.1.0.jar
