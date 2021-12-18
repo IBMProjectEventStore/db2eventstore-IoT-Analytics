7630179ca09f811f0d606fc664a1ba02f416400a
James Stroud <james.jstroud@gmail.com>
2021-12-18 03:11:45 -0500

    put echo for PY4J_VERSION

--- container/setup/setup-python.sh
+++ container/setup/setup-python.sh
@@ -27,3 +27,4 @@ rm -rf /db2eventstore-pythonpackages
 
 PY4J_VERSION=$(ls ${SPARK_HOME}/python/lib | grep py4j)
 echo "export PYTHONPATH=$PYTHONPATH:/var/lib:${SPARK_HOME}/python:${SPARK_HOME}/python/lib/${PY4J_VERSION}:${SPARK_HOME}/python/jars/:${HOME}/db2eventstore-IoT-Analytics/AdvancedApplications/PythonApplication/:${SPARK_HOME}/python/lib/py4j-0.10.7-src.zip" >> /etc/profile.d/local_python.sh
+echo "PY4J_VERSION is ${PY4J_VERSION}"
